f7b4918c7faca17847c78148408750c9b1ef91b3
Swarajpanjabi <73130501+Swarajpanjabi@users.noreply.github.com>
2022-10-23 19:58:27 +0530

    Add files via upload

---
+++ PRACTICE WEB/javascript/code 1/index.js
@@ -0,0 +1,9 @@
+// var arr = [1, 2, 3, 4, 5, 6, 7];
+// console.log(arr);
+// for (var i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// // arr.forEach(function (element) {
+// //   console.log(element);
+// // });
